f8538cb0f9ac5f9f53a4a81203bf5eea03d60f92
Lara_Magalhães_Souza <156015825+Dominique0000007@users.noreply.github.com>
2025-05-15 17:10:50 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Olá mundo! Se funcionar, vai aparecer isso 👋</Text>
+    </View>
+  );
+}
+
